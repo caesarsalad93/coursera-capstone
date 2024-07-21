73b905ede1d57e25f2f2184c233382d0bb4b1479
Alexander Huynh <alexhuynher@gmail.com>
2024-07-20 21:01:01 -0700

    add semantic html to components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,31 @@
+import footerImage from "../assets/restauranfood.jpg";
+
 const Footer = () => {
     return (
         <footer>
-            This is my footer
+            <div>
+                <img className="footerImage" src={footerImage} alt="image of food" width="200" height="250"></img>
+            </div>
+           
+            <div className="doormat">
+                <ul className="doormatList">
+                    <li><a href="./home">HOME</a></li>
+                    <li><a href="./home">ABOUT</a></li>
+                    <li><a href="./home">MENU</a></li>
+                    <li><a href="./home">RESERVATIONS</a></li>
+                    <li><a href="./home">ORDER ONLINE</a></li>
+                    <li><a href="./home">LOGIN</a></li>
+                </ul>
+            </div>
+            <div className="contactsList">
+                <span>Contact</span>
+                <span>247 Tuscany Drive, Naples, FL 34109, USA</span>
+                <span>contact@littlelemonrestaurant.com</span>
+                <span>(239) 555-0198</span>
+            </div>
+            <div className="socialsList">
+                <span>Social Media Links</span>
+            </div>
         </footer>
     )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,17 @@
+import logo from "../assets/Logo.svg"
+
 const Nav = () => {
     return (
         <nav>
-            This is my Nav
+            <img src={logo} alt="Little Lemon Logo"></img>
+            <ul>
+                <li><a href="./home">HOME</a></li>
+                <li><a href="./home">ABOUT</a></li>
+                <li><a href="./home">MENU</a></li>
+                <li><a href="./home">RESERVATIONS</a></li>
+                <li><a href="./home">ORDER ONLINE</a></li>
+                <li><a href="./home">LOGIN</a></li>
+            </ul>
         </nav>
     )
 }

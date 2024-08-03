1409f26ccaf3e6dd55de3ba609a49edf403326e2
Alexander Huynh <alexhuynher@gmail.com>
2024-08-03 13:38:50 -0700

    add h2, aria attributes, and name attributes to BookingForm

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,19 +1,17 @@
 import { useState, useEffect } from "react";
 
-const BookingForm = ({availableTimes, dispatch, submitForm}) => {
+const BookingForm = ({ availableTimes, dispatch, submitForm }) => {
   const [date, setDate] = useState("");
   const [time, setTime] = useState("17:00");
   const [guests, setGuests] = useState(1);
   const [occasion, setOccasion] = useState("Birthday");
 
   const handleDateChange = (e) => {
-   setDate(e.target.value);
-   dispatch({ type: 'UPDATE_TIMES', payload: e.target.value });
- };
-  
-useEffect(() => {
+    setDate(e.target.value);
+    dispatch({ type: "UPDATE_TIMES", payload: e.target.value });
+  };
 
-})
+  useEffect(() => {});
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -30,29 +28,35 @@ useEffect(() => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} aria-labelledby="booking-form-title">
+           <h2 id="booking-form-title">Reservation Booking</h2>
       <label htmlFor="res-date">Choose date</label>
       <input
         value={date}
         onChange={handleDateChange}
         type="date"
         id="res-date"
+        name="res-date"
         required
+        aria-required="true"
       ></input>
       <label htmlFor="res-time">Choose time</label>
       <select
         id="res-time"
+        name="res-time"
         value={time}
         onChange={(e) => {
           setTime(e.target.value);
         }}
         required
+        aria-required="true"
       >
-         {availableTimes && availableTimes.map((time) => (
+        {availableTimes &&
+          availableTimes.map((time) => (
             <option key={time} value={time}>
-               {time}
+              {time}
             </option>
-         ))}
+          ))}
       </select>
       <label htmlFor="guests">Number of guests</label>
       <input
@@ -65,6 +69,7 @@ useEffect(() => {
         min="1"
         max="10"
         id="guests"
+        name="guests"
       ></input>
       <label htmlFor="occasion">Occasion</label>
       <select
@@ -73,12 +78,18 @@ useEffect(() => {
           setOccasion(e.target.value);
         }}
         id="occasion"
+        name="occasion"
       >
         <option>Birthday</option>
         <option>Anniversary</option>
         <option>Celebrating life</option>
       </select>
-      <input type="submit" value="Make Your reservation" disabled={!date}></input>
+      <button
+        type="submit"
+        value="Make Your reservation"
+        disabled={!date}
+        aria-disabled={!date}
+      >Make Your Reservation</button>
     </form>
   );
 };
